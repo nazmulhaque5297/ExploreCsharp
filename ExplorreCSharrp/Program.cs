82ea4bc62f0d96c3ebd2ce4154d1452992f6c173
nazmul5297 <nazmulhaque5297@outlook.com>
2023-04-03 15:47:26 +0600

    enum

--- ExplorreCSharrp/Program.cs
+++ ExplorreCSharrp/Program.cs
@@ -6,66 +6,49 @@ using System.Threading.Tasks;
 
 namespace ExplorreCSharrp
 {
-    public delegate void HelloFunctionDelegate(string message);
-
-
-    // this delegates are used for multitask deligates
-
-    public delegate void SampleDelegate();
-    public delegate int SampleIntDelegate();
-    public delegate void SampleParamDelegate(out int number);
+   
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            Customer[] customers = new Customer[3];
+            customers[0] = new Customer
+            {
+                Name = "Test",
+                genderInit = GenderWithInitializeValue.Unknown
+            };
+            customers[1] = new Customer
+            {
+                Name = "Test",
+                genderCustomize = GenderWithCustomizeValue.Unknown
+            };
 
-            // Explain List
+            customers[2] = new Customer
+            {
+                Name = "Nazmul",
+                genderCustomize = GenderWithCustomizeValue.Male,
+                genderInit = GenderWithInitializeValue.Male
+            };
 
-            ExplainList explainList= new ExplainList();
-            explainList.findEvenNumber();
-           
-            // use of Delegate 
+            int[] valueOfGenderWithInitializeValue = Customer.GenderWithInitializeValue();
+            int[] valueOfWithInitializeValue = Customer.getGenderWithCustomizeValue();
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("The Name of the Customer is : {0},Gender is :{1}",customer.Name,customer.genderInit);
 
-            HelloFunctionDelegate del = new HelloFunctionDelegate(ExplainDelegate.HelloMessage);
-            del("Hi this is a call from Delegate");
+            }
 
+            foreach (int value in valueOfGenderWithInitializeValue) {
+                Console.WriteLine(value);
+            }
 
-            //Use Of Delegate 2
+            foreach (int value in valueOfWithInitializeValue)
+            {
+                Console.WriteLine(value);
+            }
 
-            List<Employee> employees = new List<Employee>();
-            employees.Add(new Employee() { Id=101,Name="Nazmul",salary=35000,experience=2});
-            employees.Add(new Employee() { Id=101,Name="Sahinur",salary=45000,experience=3});
-            employees.Add(new Employee() { Id=101,Name="Mostoba",salary=30000,experience=1});
-            employees.Add(new Employee() { Id=101,Name="Aminur",salary=700000,experience=5});
-
-            Employee.PromoteEmployee(employees, emp => emp.experience >= 5);
-
-
-            // explame of Multicast Deligate 
-
-            SampleDelegate multicastVoid = new SampleDelegate(MultiCastDeligate.sampleMethodOne);
-            multicastVoid += MultiCastDeligate.sampleMethodTwo;
-            multicastVoid();
-
-            SampleIntDelegate multiCastInt = new SampleIntDelegate(MultiCastDeligate.sampleIntMethodOne);
-            multiCastInt += MultiCastDeligate.sampleIntMethodTwo;
-            int valueInt = multiCastInt();
-
-            int param = -1;
-
-            SampleParamDelegate multiCastParam = new SampleParamDelegate(MultiCastDeligate.sampleParamMethodOne);
-            multiCastParam += MultiCastDeligate.sampleParamMethodTwo;
-            multiCastParam(out param);
-
-            Console.WriteLine("The MultiCasting Value Of the frame is {0}", param);
-
-
-
-
-
-
-        Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
