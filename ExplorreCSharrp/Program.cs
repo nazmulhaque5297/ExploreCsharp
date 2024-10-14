d1f498bcb1c03539a35c715a562889a09574cd44
nazmulhaque5297 <nazmulhaque5297@outlook.com>
2024-10-14 14:21:37 +0600

    update

--- ExplorreCSharrp/Program.cs
+++ ExplorreCSharrp/Program.cs
@@ -11,44 +11,6 @@ namespace ExplorreCSharrp
     {
         static void Main(string[] args)
         {
-            /*
-
-            Customer[] customers = new Customer[3];
-            customers[0] = new Customer
-            {
-                Name = "Test",
-                genderInit = GenderWithInitializeValue.Unknown
-            };
-            customers[1] = new Customer
-            {
-                Name = "Test",
-                genderCustomize = GenderWithCustomizeValue.Unknown
-            };
-
-            customers[2] = new Customer
-            {
-                Name = "Nazmul",
-                genderCustomize = GenderWithCustomizeValue.Male,
-                genderInit = GenderWithInitializeValue.Male
-            };
-
-            int[] valueOfGenderWithInitializeValue = Customer.GenderWithInitializeValue();
-            int[] valueOfWithInitializeValue = Customer.getGenderWithCustomizeValue();
-            foreach (Customer customer in customers)
-            {
-                Console.WriteLine("The Name of the Customer is : {0},Gender is :{1}",customer.Name,customer.genderInit);
-
-            }
-
-            foreach (int value in valueOfGenderWithInitializeValue) {
-                Console.WriteLine(value);
-            }
-
-            foreach (int value in valueOfWithInitializeValue)
-            {
-                Console.WriteLine(value);
-            } */
-
             MethodHidding mh = new MethodHidding();
             mh.DisplayEmployeeName();
 
