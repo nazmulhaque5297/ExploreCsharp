9542ee68574f788f38d3875b05b6114551c1b843
nazmulhaque5297 <nazmulhaque5297@outlook.com>
2024-11-24 15:48:20 +0600

    add uses delegate

--- ExplorreCSharrp/Deligate.cs
+++ ExplorreCSharrp/Deligate.cs
@@ -4,16 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// delegate are the type safe function pointer that means a delegate point a function where its type always be bound 
+//suppose we have 5 employee and we need to promote those whoes experience more than 5 years or eqal
 
 namespace ExplorreCSharrp
 {
-    
-    class Deligate
+
+
+    public delegate bool IsAvailToPromoted(Employee employee);
+    class UsesOfDelegate
     {
-        public void helloPrinter (string msg)
+        public static bool IsPermitToPromoted(Employee employee)
         {
-            Console.WriteLine(msg);
+            if (employee.experience >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+    }
+
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int experience { get; set; }
+
+
+        public static void printPromotedList(List<Employee> employeelist)
+        {
+            foreach (Employee employee in employeelist)
+            {
+                if (employee.experience >= 5)
+                {
+                    Console.WriteLine("{0} is Promoted", employee.Name);
+                }
+            }
+        }
+        public static void printPromotedList(List<Employee> employeelist, IsAvailToPromoted isPromoted)
+        {
+            foreach (Employee employee in employeelist)
+            {
+                if (isPromoted(employee))
+                {
+                    Console.WriteLine("{0} is Promoted", employee.Name);
+                }
+            }
+        }
+
     }
 }
