64bc85c4a80ce68f70bff97b6fad42ff86f3d258
nazmulhaque5297 <nazmulhaque5297@outlook.com>
2024-11-24 15:45:22 +0600

    add uses delegate

--- ExplorreCSharrp/Program.cs
+++ ExplorreCSharrp/Program.cs
@@ -20,6 +20,27 @@ namespace ExplorreCSharrp
             helloPrinterDeligate d = new helloPrinterDeligate(de.helloPrinter);
             d("hi this is nazmul call from a delegeate");
 
+            List<Employee> empList = new List<Employee>();
+
+            empList.Add(new Employee() { Name="Nazmul",Age=25,experience=5});
+            empList.Add(new Employee() { Name = "Sahadat", Age = 25, experience = 5 });
+            empList.Add(new Employee() { Name = "Waiji", Age = 25, experience = 5 });
+            empList.Add(new Employee() { Name = "Sadman", Age = 25, experience = 6 });
+            empList.Add(new Employee() { Name = "Rakib", Age = 25, experience = 4});
+            empList.Add(new Employee() { Name = "Taukir", Age = 25, experience = 3 });
+
+            //Employee.printPromotedList(empList);
+
+            // Here Employee.printPromotedList(empList); is call without delegate So if we want to make another 
+            //condition then we need to change the codition under the printPromotedList
+
+
+
+            // here the examle of delegates 
+
+            IsAvailToPromoted isReadyToPromote = new IsAvailToPromoted(UsesOfDelegate.IsPermitToPromoted);
+            Employee.printPromotedList(empList, isReadyToPromote);
+
             Console.ReadLine();
         }
     }
---
+++ ExplorreCSharrp/UsesOfDelegate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//suppose we have 5 employee and we need to promote those whoes experience more than 5 years or eqal
+
+namespace ExplorreCSharrp
+{
+
+
+    public delegate bool IsAvailToPromoted(Employee employee); 
+    class UsesOfDelegate
+    {
+       
+        public static bool IsPermitToPromoted(Employee employee)
+        {
+            if (employee.experience >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int experience { get; set; }
+
+
+        public static void printPromotedList(List<Employee> employeelist)
+        {
+            foreach (Employee employee in employeelist)
+            {
+                if (employee.experience >= 5)
+                {
+                    Console.WriteLine("{0} is Promoted", employee.Name);
+                }
+            }
+        }
+        public static void printPromotedList (List<Employee> employeelist, IsAvailToPromoted isPromoted)
+        {
+            foreach(Employee employee in employeelist)
+            {
+                if (isPromoted(employee))
+                {
+                    Console.WriteLine("{0} is Promoted", employee.Name);
+                }
+            }
+        }
+
+    }
+}
