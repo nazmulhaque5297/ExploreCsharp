940be1bb8ac1f8afcfc1e9778330d11aa53f03bf
nazmulhaque5297 <nazmulhaque5297@outlook.com>
2024-11-24 11:23:01 +0600

    add delegate

---
+++ ExplorreCSharrp/Deligate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// delegate are the type safe function pointer that means a delegate point a function where its type always be bound 
+
+namespace ExplorreCSharrp
+{
+    
+    class Deligate
+    {
+        public void helloPrinter (string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
--- ExplorreCSharrp/Program.cs
+++ ExplorreCSharrp/Program.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace ExplorreCSharrp
 {
-   
+    // ho here i introduce a helloPrinterDelegiate Where it is a void type cast and
+    // it can point a function which return type is void and can take string as a parameteer 
+    public delegate void helloPrinterDeligate(string ms);
     internal class Program
     {
+
+        
         static void Main(string[] args)
         {
             /*
@@ -54,10 +58,17 @@ namespace ExplorreCSharrp
             Polymorphism pm = new Polymorphism();
             pm.DisplayEmployeeName();
 
-            */
+        
 
             Properties p = new Properties();
             p.ViewProPerties();
+
+                */
+
+
+            Deligate de = new Deligate();
+            helloPrinterDeligate d = new helloPrinterDeligate(de.helloPrinter);
+            d("hi this is nazmul call from a delegeate");
 
             Console.ReadLine();
         }
